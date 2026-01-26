e73ec28ae259c470c413467752ecfd5f2c812abd
Klaywin Ryan <klaywindias@gmail.com>
2026-01-26 16:43:07 -0300

    Create abstract_purchase.py

---
+++ project/abstracts/abstract_purchase.py
@@ -0,0 +1,15 @@
+
+from ..interfaces.interface_purchase import InterfacePurchase
+from abc import ABC
+from datetime import datetime
+
+class AbstractPurchase(InterfacePurchase, ABC):
+    def __init__(self, client, seller, product, quantity_pallets):
+        self._client = client
+        self._seller = seller
+        self._product = product
+        self._quantity_pallets = quantity_pallets
+        self._date = datetime.now()
+        self._status = "PENDING"
+        self._total_value = 0.0
+        

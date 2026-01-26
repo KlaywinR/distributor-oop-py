ae50f06c1d2a8e323bfe151dac47c216f2bdf63c
Klaywin Ryan <klaywindias@gmail.com>
2026-01-26 16:42:52 -0300

    Create abstract_manager.py

---
+++ project/abstracts/abstract_manager.py
@@ -0,0 +1,8 @@
+
+from .interface_manager import InterfaceManager
+from abc import ABC
+
+class AbstractManager(InterfaceManager, ABC):
+    def __init__(self, name, registration):
+        self._name = name 
+        self._registration = registration
